7939bcd9ee8643366c2d0318a4ee5c16e8fb69a8
gonsaje <song.jaehyun@gmail.com>
2026-06-17 20:11:10 -0400

    update logo to legacy logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,11 +9,11 @@ export function Logo() {
       className="inline-flex h-14 items-center"
     >
       <Image
-        src="/vye_pink.png"
+        src="/vye_logo_pink_f36f98.png"
         alt="vye"
-        width={824}
-        height={341}
-        className="block h-11 w-auto transition hover:-translate-y-0.5 sm:h-12"
+        width={284}
+        height={193}
+        className="block h-12 w-auto transition hover:-translate-y-0.5 sm:h-14"
         priority
       />
     </Link>
